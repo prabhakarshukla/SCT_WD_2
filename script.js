15b6e2fffcdd48ef30cc7d81d13a10cc59e57127
Prabhakar Shukla <prabhakarshukla669@gmail.com>
2026-07-03 18:27:29 +0530

    added JS variables for running time

---
+++ script.js
@@ -0,0 +1,8 @@
+const display = document.querySelector(".display");
+
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const resetBtn = document.getElementById("reset");
+const lapBtn = document.getElementById("lap");
+
+const laps = document.getElementById("laps");
