7a123fbb394eca963b1ef48c40d523a17bb3a420
Prabhakar Shukla <prabhakarshukla669@gmail.com>
2026-07-04 17:17:19 +0530

    added  Updatedisplay() function in script.js

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ let seconds = 0;
 let minutes = 0;
 
 let isRunning = false;
+function updateDisplay() {
+
+    const formattedMinutes = String(minutes).padStart(2, "0");
+
+    const formattedSeconds = String(seconds).padStart(2, "0");
+
+    const formattedMilliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent =
+        `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
+
+}
+startBtn.addEventListener("click", function () {
+
+    if (isRunning) {
+        return;
+    }
+
+    isRunning = true;
+
+    timer = setInterval(function () {
+
+        milliseconds++;
+
+        if (milliseconds === 100) {
+            milliseconds = 0;
+            seconds++;
+        }
+
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+
+        updateDisplay();
+
+    }, 10);
+
+});
